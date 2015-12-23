2a5dfe8b8df16bb0d028adffbc147c63f87f0006
JeremyDulon <jeremy.dulon@live.fr>
2015-12-23 18:35:02 +0100

    Admin - GameAdmin

---
+++ src/ESGaming/CommentBundle/ESGamingCommentBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ESGaming\CommentBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ESGamingCommentBundle extends Bundle
+{
+}
---
+++ src/ESGaming/GameBundle/Admin/GameAdmin.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace ESGaming\GameBundle\Admin;
+
+use Doctrine\ORM\EntityManager;
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Route\RouteCollection;
+
+
+class GameAdmin extends Admin
+{
+    /**
+     * @var EntityManager
+     */
+    private $em;
+
+    public function setEntityManager($entityManager)
+    {
+        $this->em = $entityManager;
+    }
+
+    protected function configureRoutes(RouteCollection $collection)
+    {
+        $collection
+            ->add('updateStatus');
+    }
+
+    protected function configureListFields(ListMapper $list)
+    {
+        $actions = array();
+        $actions['edit'] = array();
+
+        $list
+            ->addIdentifier('id',null,array('label' => 'Identifiant jeu'))
+            ->add('name',null,array('label' => 'Nom du jeu'))
+            ->add('editor',null,array('label' => 'Editeur'))
+            ->add('developer',null,array('label' => 'Developpeur'))
+            ->add('type',null,array('label' => 'Type'));
+
+        $list->add('_action', 'actions', array(
+            'actions' => $actions
+        ));
+    }
+
+    protected function configureFormFields(FormMapper $form)
+    {
+        $form
+            ->add('name',null,array('label' => 'Nom du Jeu'))
+            ->add('editor',null,array('label' => 'Editeur'))
+            ->add('developer',null,array('label' => 'Developpeur'));
+    }
+
+    // Fields to be shown on filter forms
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('name');
+    }
+
+
+//    public function createQuery($context = 'list')
+//    {
+//        $query = parent::createQuery($context);
+//
+//        $em = $query->getQueryBuilder()->getEntityManager();
+//        $em->getFilters()->disable('softdeleteable');
+//
+//        return $query;
+//    }
+//
+//    public function getObject($id)
+//    {
+//        $this->em->getFilters()->disable('softdeleteable');
+//        return parent::getObject($id);
+//    }
+
+}
