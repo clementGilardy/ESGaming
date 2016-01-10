745f5b481b9f3740235092c567a6ae18e3bb06ce
clementGilardy <gilardy.clement@gmail.com>
2016-01-10 12:15:46 +0100

    add template for all bundle

--- src/ESGaming/GameBundle/Controller/DefaultController.php
+++ src/ESGaming/GameBundle/Controller/DefaultController.php
@@ -6,8 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('ESGamingGameBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('ESGamingGameBundle:Default:index.html.twig');
     }
 }
