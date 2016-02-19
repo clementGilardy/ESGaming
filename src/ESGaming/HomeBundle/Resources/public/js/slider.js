db13e7f98f67cd94558fe552529a21efb9075d3f
clementGilardy <gilardy.clement@gmail.com>
2016-02-19 11:09:52 +0100

    change

--- src/ESGaming/HomeBundle/Resources/public/js/slider.js
+++ src/ESGaming/HomeBundle/Resources/public/js/slider.js
@@ -1,7 +1,7 @@
 /**
  * Created by Clément on 11/01/2016.
  */
-$('.slider').slider({full_width: false});
+$('.slider').slider({full_width: true});
 
 $('ul.indicators').css('position','relative');
 $('div.slider').css('height','500px');
